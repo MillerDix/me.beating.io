8dcd7633db038967d0b5ceec94902380bb681304
MillerD <zhangliangwu@4snow.com>
2017-10-09 19:32:32 +0800

    prod css-module config, app title, app description

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import Routes from './routes.js';
 import registerServiceWorker from './registerServiceWorker';
+import {unregister} from './registerServiceWorker';
 
 ReactDOM.render(<Routes />, document.getElementById('root'));
 registerServiceWorker();
