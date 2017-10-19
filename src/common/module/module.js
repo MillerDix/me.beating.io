d9f96673f1a7a54fa3ce17c5c73ac1b9eb08c1d1
MillerDix <zlw2579@gmail.com>
2017-10-20 00:04:53 +0800

    module refactor

--- src/common/module/module.js
+++ src/common/module/module.js
@@ -4,9 +4,8 @@ import style from './module.css';
 const Module = props => {
 
     return (
-        <div className={style.container}>
-            <img src={props.image} alt=""/>
-            <div className={style.content}>
+        <div className={style.container} style={{backgroundImage: "url("+ props.image +")"}}>
+            <div className={style.mask}>
                 <div className={props.image ? style.titleImgBack : style.titleBlankBack}>{props.title}</div>
                 <div className={props.image ? style.subtitleImgBack : style.subtitleBlankBack}>{props.subtitle}</div>
                 <div className={style.children}>
