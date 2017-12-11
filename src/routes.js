b8819b6813d04e6448361ee1d91c8607de0e21e9
MillerD <zhangliangwu@4snow.com>
2017-12-11 15:32:41 +0800

    navbar の action with width-smaller-than-1024px screen

--- src/routes.js
+++ src/routes.js
@@ -12,32 +12,39 @@ import {Minimal} from './common/button/button.js';
 
 const history = createBrowserHistory();
 
-const Routes = () => (
-  <div>
-    <div className="home_header">
-      <div className="navlogo">Beating</div>
-      <div className="navbar">
-        <Minimal >HOME</Minimal>
-        <Minimal href="http://music.beating.io">MUSIC</Minimal>
-        <Minimal href="http://map.beating.io">MAP</Minimal>
-        <Minimal href="http://tv.beating.io">TV</Minimal>
-        <Minimal href="http://admin.beating.io">ADMIN</Minimal>
-        <Minimal href="http://me.beating.io">ABOUT ME</Minimal>
+const Routes = () => {
+
+  const toggle = () => {
+    console.log('toggle');
+  };
+
+  return (
+    <div>
+      <div className="home_header">
+        <div className="navlogo">Beating</div>
+        <div className="navbar">
+          <Minimal >HOME</Minimal>
+          <Minimal href="http://music.beating.io">MUSIC</Minimal>
+          <Minimal href="http://map.beating.io">MAP</Minimal>
+          <Minimal href="http://tv.beating.io">TV</Minimal>
+          <Minimal href="http://admin.beating.io">ADMIN</Minimal>
+          <Minimal href="http://me.beating.io">ABOUT ME</Minimal>
+        </div>
+        <div className="navToggle" onClick={() => toggle()}><div className="bar"></div></div>
       </div>
-      <div className="navToggle"><div className="bar"></div></div>
+      <div className="container">
+        <Router history={history} >
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route exact path="/articles/detail" component={ArtcDetail} />
+          </Switch>
+        </Router>
+      </div>
+      <div className="footer">
+        <div className="copyright">© 2017 MillerD. All rights are not reserved.Developed by MillerD</div>
+      </div>
     </div>
-    <div className="container">
-      <Router history={history} >
-        <Switch>
-          <Route exact path="/" component={Home} />
-          <Route exact path="/articles/detail" component={ArtcDetail} />
-        </Switch>
-      </Router>
-    </div>
-    <div className="footer">
-      <div className="copyright">© 2017 MillerD. All rights are not reserved.Developed by MillerD</div>
-    </div>
-  </div>
-)
+  );
+};
 
 export default Routes;
