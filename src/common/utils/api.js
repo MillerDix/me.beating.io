2fba5cf077b6bb91da06e01ede9b508d564c578f
MillerD <zhangliangwu@4snow.com>
2018-01-09 19:04:59 +0800

    delete local images

--- src/common/utils/api.js
+++ src/common/utils/api.js
@@ -1,5 +1,5 @@
-const URL = "http://www.beating.io:8080";
-// const URL = "http://localhost:8080";
+// const URL = "http://www.beating.io:8080";
+const URL = "http://localhost:8080";
 
 export default {
   article: {
