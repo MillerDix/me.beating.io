7d36dce674a8ad8ab4213635f0327e46cd2611c3
MillerD <millerd@MillerDdeMBP.lan>
2018-07-13 00:18:18 +0800

    rewrite api router

--- src/common/utils/api.js
+++ src/common/utils/api.js
@@ -1,10 +1,11 @@
-// const URL = "http://www.beating.io:8080";
-const URL = "http://localhost:8080";
-
 export default {
   article: {
-    listall: `${URL}/articles/all`,
-    detail: `${URL}/articles/detail`
+    listall: "/api/blog/all",
+    detail: "/api/blog/detail"
+  },
+  vision: {
+    landmarks: "/api/vision/landmarks",
+    web: "/api/vision/web"
   },
   assets: {
     images: {
